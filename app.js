3e782f26c5c958638a629d5a708e1d7990b41102
kellymaraazevedo <kellymaraazevedo@gmail.com>
2025-01-19 10:24:55 -0400

    Remove comentários de app.js

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 
 
-    // Lidar com o envio do formulário
-    document.getElementById("formMensagem").addEventListener("submit", function(event) {
-        event.preventDefault();  // Impede o envio do formulário e recarregamento da página
     
-        // Pega a mensagem do campo de texto
+    document.getElementById("formMensagem").addEventListener("submit", function(event) {
+        event.preventDefault();  
+    
+        
         const mensagem = document.getElementById("mensagem").value;
 
       
-        // Exibe a confirmação na tela
+        
         const mensagemSalvaDiv = document.getElementById("mensagemSalva");
         mensagemSalvaDiv.style.display = "block";
         
-        // Limpa o campo de mensagem
+        
         document.getElementById("mensagem").value = "";
 
-        // Oculta a confirmação após 3 segundos
+        
         setTimeout(function() {
             mensagemSalvaDiv.style.display = "none";
-        }, 3000); // A confirmação desaparece após 3 segundos
+        }, 3000); 
     });
